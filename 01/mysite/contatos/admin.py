d986ec18795caeb3443dac31747774538ed1f99a
marcoroberto891 <marcorjfaria@gmail.com>
2017-09-18 20:52:43 -0300

    segundo commit

--- 01/mysite/contatos/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-from contatos.models import Pessoa
-
-admin.site.register(Pessoa)
-# Register your models here.
--- 01/mysite/contatos/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-from django.http import HttpResponse
-
-def index(request):
-    return  HttpResponse("Hey:)")
-def contato(request):
-    return  HttpResponse("Marco Roberto")
-# Create your views here.
